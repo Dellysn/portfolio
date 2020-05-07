c8ebec80be21d91a7dced1e24036225b7c24003f
Isiaka Lukman Bamidele <isiakalukmandellyson@gmail.com>
2020-05-07 23:34:12 +0100

    fixed some typos

--- js/main.js
+++ js/main.js
@@ -25,7 +25,35 @@ function eventHandler(id, event) {
 eventHandler("contact-button", displayContact);
 eventHandler("blog-button", displayBlog);
 eventHandler("about-button", displayAbout);
+let mapAndDisplay = function (string, id) {
+  let strings = string
+    .map(function (str) {
+      return `
+    <span class="jingle">${str}</span>
+  `;
+    })
+    .join("");
+  document.getElementById(id).innerHTML = strings;
+};
 
+const nickname = "DELLYSON";
+const fullname = " Isiaka Lukman Bamidele";
+
+const nicknameArr = Array.from(nickname);
+const fullnameArr = Array.from(fullname);
+mapAndDisplay(nicknameArr, "name-display");
+
+// let nickname = formatDellyson
+//   .map(function (dellyson, index) {
+//     return `
+//     <span class="jingle">${dellyson}</span>
+//   `;
+//   })
+//   .join("");
+
+// map and display
+
+// displayNickname.innerHTML = nickname;
 // buttons.forEach(function (button) {
 //   button.addEventListener("click", function () {
 //     let current = document.getElementsByClassName("active");
